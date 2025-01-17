14ae2210d2182fd76c42289c44948386ac777d64
anujisdead <149852140+anujisdead@users.noreply.github.com>
2025-01-17 07:59:31 +0530

    Add files via upload
    You will need to have an additional venv folder inorder to include the Phidata and Groq API keys

---
+++ financial_agent.py
@@ -0,0 +1,22 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.googlesearch import GoogleSearch
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+## web search agent
+web_search_agent=Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Alway include sources"],
+    show_tools_calls=True,
+    markdown=True,
+
+)
